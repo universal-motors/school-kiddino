c00f1a43b68506c09c725a49f87c360579d912d8
razaahmad11 <razaqazhi@gmail.com>
2023-12-08 18:57:39 +0500

    fix: learning section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 import HeroSection from "@/components/heroSection";
 // import BackgroudImage from "@/app/assets/images/bg/1.jpg";
+import Image from "next/image";
 import { Box } from "@mui/material";
+import image1 from "@/assets/images/ab-1-1.jpg";
+import image2 from "@/assets/images/ab-1-2.jpg";
+import image3 from "@/assets/images/ab-1-3.jpg";
+import image4 from "@/assets/images/ab-1-4.jpg";
+import ChaletIcon from "@mui/icons-material/Chalet";
+import Leadlearning from "@/components/leadlearning";
 export default function HomePage() {
   return (
     <Box>
       <HeroSection />
-      homepage
+      <Leadlearning />
     </Box>
   );
 }
