08041aac86e78b54e24e910f7c1ffacc418e0b9f
razaahmad11 <razaqazhi@gmail.com>
2023-12-12 16:25:26 +0500

    fix: creaed toogle menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,13 +6,9 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SearchIcon from "@mui/icons-material/Search";
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
-import MenuIcon from "@mui/icons-material/Menu";
-import Image from "next/image";
-import coverImage from "@/assets/images/coverImage.jpg";
 import TemporaryDrawer from "../menu";
 export default function Header() {
   const [isOpen, setMenu] = useState(false);
-  const toggleMenu = () => {};
   return (
     <div>
       <div>
@@ -67,10 +63,7 @@ export default function Header() {
             <div className="hover:text-[#BC0B54] cursor-pointer">Contact</div>
           </div>
           <div className="flex mr-1 sm:mr-10 md:mr-20 lg:gap-4  items-center">
-            <div
-              onClick={toggleMenu}
-              className="!bg-[#E8063C] lg:!bg-[#ffffff]  rounded-full text-[#ffffff] lg:text-[black] lg:hover:text-[#E8063C] "
-            >
+            <div>
               <TemporaryDrawer />
             </div>
             <div className="text-[14px] xl:text-[16px] !hidden lg:!flex  bg-[#E8063C] hover:bg-[#4D0D58] border-[#E8063C] border-[3px] px-2 xl:px-6 py-2 xl:py-3 font-semibold text-[#ffffff] rounded-full">
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,6 +2,12 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import CloseIcon from "@mui/icons-material/Close";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import AddIcon from "@mui/icons-material/Add";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
@@ -11,12 +17,14 @@ import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Anchor } from "@mui/icons-material";
 
-type Anchor = "right";
+type Anchor = "right" | "left";
 
 export default function TemporaryDrawer() {
   const [state, setState] = React.useState({
     right: false,
+    left: false,
   });
 
   const toggleDrawer =
@@ -29,67 +37,266 @@ export default function TemporaryDrawer() {
       ) {
         return;
       }
-
       setState({ ...state, [anchor]: open });
     };
-
-  const list = (anchor: Anchor) => (
-    <Box
-      sx={{ width: anchor === "right" || anchor === "right" ? "auto" : 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
   return (
-    <div className="">
-      {(["right"] as const).map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>
-            <MenuIcon
-              sx={{
-                fontSize: "35px",
-                color: "black",
-                ":hover": { color: "white", background: "#E8063C" },
-              }}
-            />
-          </Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
+    <div>
+      <div className="!hidden lg:!flex">
+        {(["right"] as const).map((anchor) => (
+          <React.Fragment key={anchor}>
+            <Button onClick={toggleDrawer(anchor, true)}>
+              <MenuIcon
+                sx={{
+                  fontSize: "35px",
+                  color: "black",
+                  ":hover": { color: "#E8063C" },
+                }}
+              />
+            </Button>
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+              <div className="!hidden lg:!flex">
+                <Box sx={{ width: 500 }}>
+                  <div className="flex items-end justify-end bg-[#E8063C] text-[#ffffff] w-6 rounded-full mt-2 ml-3">
+                    <CloseIcon />
+                  </div>
+                  <div className="pl-14 pt-10">
+                    <div>
+                      <img
+                        src="https://html.vecurosoft.com/kiddino/demo/assets/img/logo.svg"
+                        alt=""
+                      />
+                    </div>
+                    <div className="text-[black] text-[14px] font-normal mt-5">
+                      We are constantly expanding the range of services <br />
+                      offered, taking care of children of all ages.
+                    </div>
+                    <div className="text-[30px] font-bold mt-8">
+                      Get In Touch
+                    </div>
+                    <div className="mt-6">
+                      Monday to Friday: <span>8.30am – 02.00pm</span>
+                    </div>
+                    <div className="mt-3">
+                      Saturday, Sunday:{" "}
+                      <span className="font-semibold">Close</span>
+                    </div>
+                    <div className="flex items-center gap-3 mt-6">
+                      <div className="rounded-full h-10 w-10 flex justify-center items-center bg-[#FFD600] hover:border-none ">
+                        <EmailIcon />
+                      </div>
+                      <div>
+                        Email:{" "}
+                        <span className="hover:text-[#EC063C] cursor-pointer">
+                          user@domainname.com
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-3 mt-6">
+                      <div className="rounded-full h-10 w-10 flex justify-center items-center bg-[#FFD600] hover:border-none ">
+                        <PhoneIphoneIcon />
+                      </div>
+                      <div>
+                        Phone:{" "}
+                        <span className="hover:text-[#EC063C] cursor-pointer">
+                          +44 (0) 207 689 7888
+                        </span>
+                      </div>
+                    </div>
+                    <div className="text-[30px] font-bold mt-8">
+                      Latest News
+                    </div>
+                    <div className="flex items-center mt-6 gap-4">
+                      <div>
+                        <img
+                          className="rounded-2xl"
+                          src="https://html.vecurosoft.com/kiddino/demo/assets/img/blog/recent-post-1-1.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="flex flex-col">
+                        <div className="flex gap-1 text-[#E8063C] hover:text-[black] items-center">
+                          <div>
+                            <CalendarMonthIcon />
+                          </div>
+                          <div>December 3, 2022</div>
+                        </div>
+                        <div className="text-[20px] font-bold hover:text-[#E8063C]">
+                          A very warm welcome to our <br />
+                          new Treasurer
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center mt-3 gap-4">
+                      <div>
+                        <img
+                          className="rounded-2xl"
+                          src="https://html.vecurosoft.com/kiddino/demo/assets/img/blog/recent-post-1-2.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="flex flex-col">
+                        <div className="flex gap-1 text-[#E8063C] hover:text-[black] items-center">
+                          <div>
+                            <CalendarMonthIcon />
+                          </div>
+                          <div>February 15, 2022</div>
+                        </div>
+                        <div className="text-[20px] font-bold hover:text-[#E8063C]">
+                          German kinder and garten <br />
+                          mean child
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center mt-3 gap-4">
+                      <div>
+                        <img
+                          className="rounded-2xl"
+                          src="https://html.vecurosoft.com/kiddino/demo/assets/img/blog/recent-post-1-3.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="flex flex-col">
+                        <div className="flex gap-1 text-[#E8063C] hover:text-[black] items-center">
+                          <div>
+                            <CalendarMonthIcon />
+                          </div>
+                          <div>Augest 20, 2022</div>
+                        </div>
+                        <div className="text-[20px] font-bold hover:text-[#E8063C]">
+                          English uses term to refer to <br />
+                          the earliest
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Box>
+              </div>
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="!flex lg:!hidden">
+        {(["left"] as const).map((anchor) => (
+          <React.Fragment key={anchor}>
+            <Button onClick={toggleDrawer(anchor, true)}>
+              <MenuIcon
+                sx={{
+                  fontSize: "35px",
+                  color: "black",
+                  ":hover": { color: "#E8063C" },
+                }}
+              />
+            </Button>
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+              {/* {list(anchor)} */}
+              <div>
+                <Box sx={{ width: 300 }}>
+                  <div className="flex items-end justify-end bg-[#E8063C] text-[#ffffff] w-6 rounded-full mt-2 ml-3">
+                    <CloseIcon />
+                  </div>
+                  <div className="text-center">
+                    <div className="flex justify-center mt-10">
+                      <img
+                        src="https://html.vecurosoft.com/kiddino/demo/assets/img/logo.svg"
+                        alt=""
+                      />
+                    </div>
+                    <div className="mt-10">
+                      <div className="flex justify-between ml-7 mr-7">
+                        <div className="flex gap-2 items-center">
+                          <div>
+                            <ArrowForwardIosIcon style={{ fontSize: "15px" }} />
+                          </div>
+                          <div className="font-extrabold">Demo</div>
+                        </div>
+                        <div className="bg-[#490D59] flex items-center justify-center text-[#ffffff]  w-8 h-8 rounded-full">
+                          <AddIcon />
+                        </div>
+                      </div>
+                      <hr className="w-[80%] ml-8 mt-3" />
+                    </div>
+                    <div className="mt-3">
+                      <div className="flex justify-between ml-7 mr-7">
+                        <div className="flex gap-2 items-center">
+                          <div>
+                            <ArrowForwardIosIcon style={{ fontSize: "15px" }} />
+                          </div>
+                          <div className="font-extrabold">About Us</div>
+                        </div>
+                      </div>
+                      <hr className="w-[80%] ml-8 mt-3" />
+                    </div>
+                    <div className="mt-3">
+                      <div className="flex justify-between ml-7 mr-7">
+                        <div className="flex gap-2 items-center">
+                          <div>
+                            <ArrowForwardIosIcon style={{ fontSize: "15px" }} />
+                          </div>
+                          <div className="font-extrabold">Classes</div>
+                        </div>
+                        <div className="bg-[#490D59] flex items-center justify-center text-[#ffffff]  w-8 h-8 rounded-full">
+                          <AddIcon />
+                        </div>
+                      </div>
+                      <hr className="w-[80%] ml-8 mt-3" />
+                    </div>
+                    <div className="mt-3">
+                      <div className="flex justify-between ml-7 mr-7">
+                        <div className="flex gap-2 items-center">
+                          <div>
+                            <ArrowForwardIosIcon style={{ fontSize: "15px" }} />
+                          </div>
+                          <div className="font-extrabold">Blogs</div>
+                        </div>
+                        <div className="bg-[#490D59] flex items-center justify-center text-[#ffffff]  w-8 h-8 rounded-full">
+                          <AddIcon />
+                        </div>
+                      </div>
+                      <hr className="w-[80%] ml-8 mt-3" />
+                    </div>
+                    <div className="mt-3">
+                      <div className="flex justify-between ml-7 mr-7">
+                        <div className="flex gap-2 items-center">
+                          <div>
+                            <ArrowForwardIosIcon style={{ fontSize: "15px" }} />
+                          </div>
+                          <div className="font-extrabold">Pages</div>
+                        </div>
+                        <div className="bg-[#490D59] flex items-center justify-center text-[#ffffff]  w-8 h-8 rounded-full">
+                          <AddIcon />
+                        </div>
+                      </div>
+                      <hr className="w-[80%] ml-8 mt-3" />
+                    </div>
+                    <div className="mt-3">
+                      <div className="flex justify-between ml-7 mr-7">
+                        <div className="flex gap-2 items-center">
+                          <div>
+                            <ArrowForwardIosIcon style={{ fontSize: "15px" }} />
+                          </div>
+                          <div className="font-extrabold">Contact Us</div>
+                        </div>
+                        <div className="bg-[#490D59] flex items-center justify-center text-[#ffffff]  w-8 h-8 rounded-full">
+                          <AddIcon />
+                        </div>
+                      </div>
+                      <hr className="w-[80%] ml-8 mt-3" />
+                    </div>
+                  </div>
+                </Box>
+              </div>
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
